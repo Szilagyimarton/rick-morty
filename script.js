d7416b5577cdabcb655ae1d8ce277593e8573820
Szilagyimarton <szilagyimarton92@gmail.com>
2023-11-10 14:25:51 +0100

    sidebar added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const fetchUrl = (url) => fetch(url).then((res) => res.json());
 
 const skeletonComponent =() => `
 <h1>Rick & Morty App</h1>
+<p id="selected-card"></p>
 <div class="characters"></div>
 <div class="buttons"></div>`
 
@@ -16,11 +17,26 @@ const characterComponent = (characterData) => `
 const buttonComponent = (id,text) => {
   return `<button id=${id}>${text}</button>`
 }
+const selectedCharacterComponent = (chardata) =>{
+let episodeHtml =[]
+chardata.episode.forEach(episode => {
+  episodeHtml.push(episode.substring(40)) 
 
+})
+return  `
+  <h2>${chardata.name}</h2>
+  <h3>${chardata.status}</h3>
+  <h4>${chardata.gender}</h4>
+  <h5>${chardata.species}</h5>
+  <h6>${episodeHtml.join(", ")}</h6>
+  `
+}
 const buttonEventComponent = (id,url) => {
   const buttonElement = document.querySelector(`#${id}`)
   buttonElement.addEventListener("click", () => {
-    fetchUrl(url).then(data => makeDomFromData(data,rootElement))
+    fetchUrl(url).then(data => {
+      makeDomFromData(data,rootElement)
+     cardEventComponent(data)})
   })
 }
 const makeDomFromData = (data,element) => {
@@ -31,6 +47,7 @@ const makeDomFromData = (data,element) => {
   const characters = data.results;
   characters.forEach((character) => characterElement.insertAdjacentHTML("beforeend",characterComponent(character))
   );
+
   if(info.prev){
     buttonElement.insertAdjacentHTML("beforeend", buttonComponent("prev",`<span class="material-symbols-outlined">
     arrow_back
@@ -45,9 +62,20 @@ const makeDomFromData = (data,element) => {
     }
   
 }
+
+const cardEventComponent = (data) => {
+  const charElements = document.querySelectorAll(".char")
+  const selectedCardElement = document.querySelector("#selected-card")
+  charElements.forEach(element => element.addEventListener("click",()=>{
+    const selectedName = element.querySelector("h2").innerText
+    const selectedChar = data.results.find(char => char.name === selectedName)
+    selectedCardElement.innerHTML= selectedCharacterComponent(selectedChar)
+  }))
+}
 const init = () => {
   fetchUrl("https://rickandmortyapi.com/api/character").then((data) => {
    makeDomFromData(data,rootElement);
+   cardEventComponent(data)
   });
 };
 init();
